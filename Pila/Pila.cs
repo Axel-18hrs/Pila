79c9d59166968243a0658e2202e4dc49eaefb33b
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2023-09-06 20:31:55 -0600

    Agregar archivos de proyecto.

---
+++ Pila/Pila.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Pila
+{
+    internal class Pila
+    {
+        private Nodo ultimoNodo;
+        public Nodo UltimoNodo()
+        {
+            return ultimoNodo;
+        }
+        public void Apilar(Nodo newNodo)
+        {
+            if (ultimoNodo == null)
+            {
+                ultimoNodo = newNodo;
+            }
+            else
+            {
+                Nodo ultNodo = ultimoNodo;
+                ultimoNodo = newNodo;
+                ultimoNodo.Siguiente = ultNodo;
+            }
+        }
+        public void Desapiilar()
+        {
+            Console.Clear();
+            if(ultimoNodo == null)
+            {
+                Console.WriteLine("// The stack is empty");
+                Console.ReadKey();
+                Console.Clear();
+                return;
+               
+            }
+            ultimoNodo = ultimoNodo.Siguiente;
+            MostrarPila();
+            
+        }
+
+        public void MostrarPila()
+        {
+            Console.Clear();
+
+            if(ultimoNodo == null)
+            {
+                Console.WriteLine("// The stack is empty");
+                Console.ReadKey();
+                Console.Clear();
+                return;
+               
+            }
+
+            MostrarNodo(ultimoNodo);
+        }
+
+        public void MostrarNodo(Nodo nNodo)
+        {
+            Console.WriteLine(nNodo.Numero);
+
+            if(nNodo.Siguiente != null)
+            {
+                MostrarNodo(nNodo.Siguiente);
+            }
+        }
+    }
+}
---
+++ Pila/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pila
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pila pila = new Pila();
+            bool salir = true;
+
+            while (salir)
+            {
+                Console.WriteLine("// Select an option" +
+                    "\n[1]: Enter a value " +
+                    "\n[2]: Delete a value" +
+                    "\n[3]: Show stack" +
+                    "\n[4]: Exit");
+
+                int opcion;
+
+                try
+                {
+                    opcion = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.Clear();
+                    continue;
+                }
+
+                switch (opcion)
+                {
+
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Enter a value:");
+                        Nodo nodo = new Nodo();
+                        nodo.Numero = Convert.ToInt16(Console.ReadLine());
+                        pila.Apilar(nodo);
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("Number to search for:");
+                        pila.Desapiilar();
+
+                        break;
+
+                    case 3:
+                        pila.MostrarPila();
+                        break;
+
+                    case 4:
+                        salir = false;
+                        Console.Clear();
+                        Console.WriteLine("Good bye <3");
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
